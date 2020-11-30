29a1ee631c582ffa923ecc5ee8cde5df72fb1bac
jionumber <jionumber98778@gmail.com>
2020-11-30 23:35:30 +0530

    added daly ques 1

--- dailycodes.py
+++ dailycodes.py
@@ -1,1 +1,10 @@
-print("hello world")
+def my_func1():
+#ques1 WAP to enter few numbers then count the all even numbers entered.
+    a = int(input("enter the 1st number :"))
+    b = int(input("enter the 2st number :"))
+    c = int(input("enter the 3rd number :"))
+    list = [a,b,c]
+    for x in list:
+        if (x%2==0):
+            print(x)
+my_func1()
