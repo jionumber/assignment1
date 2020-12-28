7c03a4c0c22e57a6c9afc9b16316bf8ea87cde07
jionumber <jionumber98778@gmail.com>
2020-12-28 13:27:27 +0530

    qus 25 added

--- dailycodes.py
+++ dailycodes.py
@@ -279,3 +279,18 @@ def myfunc24():
 
     func()
 myfunc24()
+def myfunc25():
+    def fibonacci(n):
+        arr = [0] * (n + 1)
+
+        arr[1] = 1
+
+        for i in range(2, n + 1):
+            arr[i] = arr[i - 1] + arr[i - 2]
+
+        return arr[n]
+
+    # Driver Program
+    if __name__ == "__main__":
+        print(fibonacci(int(input("Enter the term :"))))  # lets assume the input as 12
+myfunc25()
